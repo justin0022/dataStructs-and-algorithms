67e43625cb191ebba0b62e9c7d702b178698f201
Justin Lee <justinlee0022@gmail.com>
2018-11-20 13:48:15 -0800

    Added test

--- test/test.js
+++ test/test.js
@@ -3,5 +3,8 @@ const u = require('../')
 const sort = u().sort
 
 test('quickSort sorts an array of numbers from smallest to largest', () => {
+  expect(sort.quickSort([])).toEqual([])
+  expect(sort.quickSort([1])).toEqual([1])
   expect(sort.quickSort([4,1,2])).toEqual([1,2,4])
+  expect(sort.quickSort([4,1,-5])).toEqual([-5,1,4])
 }) 
