b175409264716cbc8a3b6944b3b6f52e5150364b
Justin Lee <justinlee0022@gmail.com>
2018-11-20 14:21:09 -0800

    TDD set up for sorting algos

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -1,5 +1,5 @@
 const quickSort = a => {
-  let arr = [...a]
+  const arr = [...a]
   if (arr.length === 0) return []
   const middle = arr.shift()
   const left = arr.filter(x => x < middle)
@@ -7,6 +7,38 @@ const quickSort = a => {
   return [...quickSort(left), middle, ...quickSort(right)]
 }
 
+const mergeSort = a => {
+  const arr = [...a]
+  // todo
+
+  return arr
+}
+
+const selectionSort = a => {
+  const arr = [...a]
+  // todo
+
+  return arr
+}
+
+const bogoSort = a => {
+  const arr = [...a]
+  // todo
+
+  return arr
+}
+
+const bubbleSort = a => {
+  const arr = [...a]
+  // todo
+
+  return arr
+}
+
 module.exports = {
-  quickSort
+  quickSort,
+  mergeSort,
+  selectionSort,
+  bogoSort,
+  bubbleSort
 }
