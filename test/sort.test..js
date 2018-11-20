c39f64b0b62c1fe7ea93b1c0ced8584ec83ac701
Justin Lee <justinlee0022@gmail.com>
2018-11-20 13:58:50 -0800

    Fixed formatting

--- test/sort.test..js
+++ test/sort.test..js
@@ -7,6 +7,6 @@ const sort = u().sort
 test('quickSort sorts an array of numbers from smallest to largest', () => {
   expect(sort.quickSort([])).toEqual([])
   expect(sort.quickSort([1])).toEqual([1])
-  expect(sort.quickSort([4,1,2])).toEqual([1,2,4])
-  expect(sort.quickSort([4,1,-5])).toEqual([-5,1,4])
-}) 
+  expect(sort.quickSort([4, 1, 2])).toEqual([1, 2, 4])
+  expect(sort.quickSort([4, 1, -5])).toEqual([-5, 1, 4])
+})
