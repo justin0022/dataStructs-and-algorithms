28a71bc6b63d2076c459342bd5690ff6277fcde5
Justin Lee <justinlee0022@gmail.com>
2018-11-22 14:07:34 -0800

    nearly implemented bst

--- dataStructures/bstTree.js
+++ dataStructures/bstTree.js
@@ -5,14 +5,21 @@ const createNode = val => ({
 })
 
 const search = (node, val) => {
-  if (node === null) {
-    return null
-  } else if (val === node.val) {
-    return node
-  } else if (val < node.val) {
-    return search(node.left, val)
-  } else if (val > node.val) {
-    return search(node.right, val)
+  if (node === null) return null
+  else if (val === node.val) return node
+  else if (val < node.val) return search(node.left, val)
+  else if (val > node.val) return search(node.right, val)
+}
+
+const insertNode = (node, newNode) => {
+  if (newNode.val < node.val) {
+    if (node.left === null) {
+      node.left = newNode
+    } else insertNode(node.left, newNode)
+  } else {
+    if (node.right === null) {
+      node.right = newNode
+    } else insertNode(node.right, newNode)
   }
 }
 
@@ -21,26 +28,14 @@ const createBST = () => {
   return {
     insert: function (val) {
       const node = createNode(val)
-      const insertNode = (node, newNode) => {
-        if (newNode.val < node.val) {
-          if (node.left === null) {
-            node.left = newNode
-          } else insertNode(node.left, newNode)
-        } else {
-          if (node.right === null) {
-            node.right = newNode
-          } else insertNode(node.right, newNode)
-        }
-      }
       if (root === null) root = node
       else insertNode(root, node)
-
       return this
     },
     find: val => search(root, val),
     contains: val => search(root, val) != null,
     delete: val => {
-
+      // todo
     },
     getTree: () => root
   }
