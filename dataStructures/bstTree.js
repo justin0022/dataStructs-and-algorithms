7b63223416c996cdae0b1bd6b71fda336989eeb8
Justin Lee <justinlee0022@gmail.com>
2018-11-22 12:12:48 -0800

    Figured out this

--- dataStructures/bstTree.js
+++ dataStructures/bstTree.js
@@ -7,30 +7,25 @@ const createNode = val => ({
 const createBST = () => {
   let root = null
   return {
-    insert: val => {
+    insert: function (val) {
       const node = createNode(val)
 
       const insertNode = (node, newNode) => {
         if (newNode.val < node.data) {
           if (node.left === null) {
             node.left = newNode
-          } else {
-            insertNode(node.left, newNode)
-          }
+          } else insertNode(node.left, newNode)
         } else {
           if (node.right === null) {
             node.right = newNode
-          } else {
-            insertNode(node.right, newNode)
-          }
+          } else insertNode(node.right, newNode)
         }
       }
 
-      root === null
-        ? root = node
-        : insertNode(root, node)
+      if (root === null) root = node
+      else insertNode(root, node)
 
-      return root
+      return this
     },
     contains: () => {
       return root
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { createBST } = require('./dataStructure')
 
 const bst = createBST()
 
-console.log(bst)
+bst
+  .insert(15)
+  .insert(25)
+  .insert(10)
+  .insert(7)
 
 console.log(bst.contains())
