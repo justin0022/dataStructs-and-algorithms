69213dd8ebb250c5eb461598f2c37ebb5717f6ad
Justin Lee <justinlee0022@gmail.com>
2018-11-23 09:35:42 -0800

    Wrote more tests for find in linkedList

--- dataStructures/linkedList.js
+++ dataStructures/linkedList.js
@@ -9,24 +9,26 @@ const insertNode = (node, val) => {
   } else insertNode(node.next, val)
 }
 
+const findNode = (node, val) => {
+  if (node == null) return null
+  if (node.val === val) return node
+  else return findNode(node.next, val)
+}
+
 const createLinkedList = () => {
   let head = null
   return {
     insert: function (val) {
       const node = createNode(val)
-      if (head === null) head = node
+      if (head == null) head = node
       else insertNode(head, node)
       return this
     },
     delete: function (val) {
 
     },
-    find: () => {
-
-    },
-    contains: () => {
-
-    },
+    find: val => findNode(head, val),
+    contains: val => findNode(val) === null,
     getLinkedList: () => head
   }
 }
--- test/linkedList.test.js
+++ test/linkedList.test.js
@@ -2,7 +2,7 @@
 
 const d = require('../dataStructure.js')
 
-test('getLinkedList returns linkedlist', () => {
+test('linkedList creation works', () => {
   const linkedList = d.createLinkedList()
   expect(linkedList).toBeDefined()
   expect(linkedList.insert(1).getLinkedList()).toEqual({ val: 1, next: null })
@@ -12,4 +12,22 @@ test('getLinkedList returns linkedlist', () => {
 
   const linkedList3 = d.createLinkedList()
   expect(linkedList3.insert(1).insert(2).getLinkedList()).toEqual({ val: 1, next: { val: 2, next: null } })
+})
+
+test('linkedList find works', () => {
+  const linkedList = d.createLinkedList()
+  expect(linkedList.insert(1).find(1)).toEqual({ val: 1, next: null })
+  linkedList.insert(2)
+  expect(linkedList.find(2)).toEqual({ val: 2, next: null })
+  expect(linkedList.find(1)).toEqual({ val: 1, next: { val: 2, next: null } })
+  linkedList.insert(1)
+  expect(linkedList.find(1)).toEqual({ val: 1, next: { val: 2, next: { val: 1, next: null } } })
+})
+
+test('linkedList contains works', () => {
+
+})
+
+test('linkedList delete works', () => {
+
 })
