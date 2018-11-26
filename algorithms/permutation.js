171d88ecdf79dec8afb7d943580eda8a5c794fb9
Justin Lee <justinlee0022@gmail.com>
2018-11-26 09:20:10 -0800

    Working on iterative permutation

--- algorithms/permutation.js
+++ algorithms/permutation.js
@@ -1,7 +1,11 @@
-const permutation = ([first, ...rest]) => {
-  if (first === undefined) return []
-  if (rest.length === 0) return [first]
+const permutation = (...x) => {
+  const permutations = []
 
+  for (let i = 0; i < x.length; i++) {
+
+  }
+
+  return permutation
 }
 
 module.exports = permutation
